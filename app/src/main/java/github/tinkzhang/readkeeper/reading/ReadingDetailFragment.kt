8ff47c4964a6470f66cccac0d834ab4623c9c07a
Tink, Zhang <yunfezhang@ebay.com>
2020-10-10 16:46:12 +0800

    Update progressView after editing

--- app/src/main/java/github/tinkzhang/readkeeper/reading/ReadingDetailFragment.kt
+++ app/src/main/java/github/tinkzhang/readkeeper/reading/ReadingDetailFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.navGraphViewModels
 import com.afollestad.materialdialogs.LayoutMode
 import com.afollestad.materialdialogs.MaterialDialog
@@ -24,7 +25,6 @@ import kotlinx.android.synthetic.main.detail_progress_cardview.*
 import kotlinx.android.synthetic.main.fragment_reading_detail.*
 
 class ReadingDetailFragment : Fragment() {
-    private lateinit var readingBook: ReadingBook
     val viewModel: ReadingViewModel by navGraphViewModels(R.id.mobile_navigation) {
         ReadingViewModelFactory(this.requireActivity().application)
     }
@@ -32,59 +32,71 @@ class ReadingDetailFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        readingBook = viewModel.selectedBook!!
         return inflater.inflate(R.layout.fragment_reading_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        detail_title.text = viewModel.selectedBook?.title
-        configProgressView(readingBook.pages, readingBook.progress)
+        viewModel.selectedBook.observe(viewLifecycleOwner, Observer {
+            detail_title.text = it.title
+        })
+        configProgressView()
     }
 
-    private fun configProgressView(pages: Int, progress: Int) {
-        detailProgress.iconImageResource = R.drawable.ic_kindle
-        detailProgress.max = pages.toFloat()
-        detailProgress.progress = progress.toFloat()
+    private fun configProgressView() {
 
-        val progressHtmlString = getString(R.string.progress_html, progress, pages)
-        detailProgressTextView.text = Html.fromHtml(progressHtmlString, FROM_HTML_MODE_COMPACT)
+        viewModel.selectedBook.observe(viewLifecycleOwner, Observer {
+            detailProgress.max = it.pages.toFloat()
+            detailProgress.progress = it.progress.toFloat()
+            detailProgress.iconImageResource = it.type?.logoResId() ?: R.drawable.ic_unknown
+
+            val progressHtmlString = getString(R.string.progress_html, it.progress, it.pages)
+            detailProgressTextView.text = Html.fromHtml(progressHtmlString, FROM_HTML_MODE_COMPACT)
+        })
+
         detailProgressCardView.setOnClickListener {
             MaterialDialog(requireContext(), BottomSheet(LayoutMode.WRAP_CONTENT)).show outer@ {
                 title(R.string.update_progress)
                 cancelOnTouchOutside(false)
                 cancelable(false)
                 customView(R.layout.dialog_progress_update, scrollable = true, horizontalPadding = true)
-                this.getCustomView().findViewById<TextInputEditText>(R.id.progressEditText).apply {
-                    setText(progress.toString())
+                val pageText = this.getCustomView().findViewById<TextInputEditText>(R.id.bookPageEditText)
+                val progressText = this.getCustomView().findViewById<TextInputEditText>(R.id.progressEditText)
+                progressText.apply {
+                    setText(viewModel.selectedBook.value?.progress.toString())
                     requestFocus()
                 }
-                this.getCustomView().findViewById<TextInputEditText>(R.id.bookPageEditText).setText(pages.toString())
+                pageText.setText(viewModel.selectedBook.value?.pages.toString())
                 this.getCustomView().findViewById<CardView>(R.id.bookTypeCardView).setOnClickListener {
 
                     val bookTypeList = BookType.values().map { BasicGridItem(it.logoResId(), it.nameString()) }
 
                     MaterialDialog(this@ReadingDetailFragment.requireContext(), BottomSheet(LayoutMode.WRAP_CONTENT)).show {
                         gridItems(bookTypeList) { _, index, item ->
+                            val readingBook = viewModel.selectedBook.value
                             when (item.iconRes) {
-                                R.drawable.ic_paper_book -> readingBook.type = BookType.PAPER
-                                R.drawable.ic_kindle -> readingBook.type = BookType.KINDLE
-                                R.drawable.ic_pdf -> readingBook.type = BookType.PDF
-                                R.drawable.ic_google_book -> readingBook.type = BookType.GOOGLE
-                                R.drawable.ic_apple_book -> readingBook.type = BookType.APPLE
-                                R.drawable.ic_wechat -> readingBook.type = BookType.WECHAT
-                                R.drawable.ic_oreilly -> readingBook.type = BookType.OREILLY
-                                R.drawable.ic_ebook -> readingBook.type = BookType.EBOOK
+                                R.drawable.ic_paper_book -> readingBook?.type = BookType.PAPER
+                                R.drawable.ic_kindle -> readingBook?.type = BookType.KINDLE
+                                R.drawable.ic_pdf -> readingBook?.type = BookType.PDF
+                                R.drawable.ic_google_book -> readingBook?.type = BookType.GOOGLE
+                                R.drawable.ic_apple_book -> readingBook?.type = BookType.APPLE
+                                R.drawable.ic_wechat -> readingBook?.type = BookType.WECHAT
+                                R.drawable.ic_oreilly -> readingBook?.type = BookType.OREILLY
+                                R.drawable.ic_ebook -> readingBook?.type = BookType.EBOOK
                             }
                             this@outer.getCustomView().findViewById<ImageView>(R.id.bookTypeEditImageView)
-                                    .setImageResource(readingBook.type?.logoResId() ?: R.drawable.ic_unknown)
+                                    .setImageResource(readingBook?.type?.logoResId() ?: R.drawable.ic_unknown)
                         }
                         positiveButton(R.string.done)
                     }
                 }
                 this.getCustomView().findViewById<ImageView>(R.id.bookTypeEditImageView)
-                        .setImageResource(readingBook.type?.logoResId() ?: R.drawable.ic_unknown)
+                        .setImageResource(viewModel.selectedBook.value?.type?.logoResId() ?: R.drawable.ic_unknown)
                 positiveButton(R.string.update) { dialog ->
+                    viewModel.selectedBook.postValue(viewModel.selectedBook.value.apply {
+                        this?.progress = progressText.text.toString().toInt()
+                        this?.pages = pageText.text.toString().toInt()
+                    })
                     // Pull the password out of the custom view when the positive button is pressed
 //                    val passwordInput: EditText = dialog.getCustomView()
 //                            .findViewById(R.id.password)
--- app/src/main/java/github/tinkzhang/readkeeper/reading/ReadingViewModel.kt
+++ app/src/main/java/github/tinkzhang/readkeeper/reading/ReadingViewModel.kt
@@ -14,14 +14,15 @@ class ReadingViewModel(application: Application) : AndroidViewModel(application)
     private val repository: ReadingRepository
     val books: LiveData<List<ReadingBook>>
     var isLoading = MutableLiveData<Boolean>()
-    var selectedBook: ReadingBook? = null
-        get() = selectedBookIndex?.let { books.value?.get(it) }
-
-    val selectedBookA: MutableLiveData<ReadingBook> by lazy {
-        MutableLiveData<ReadingBook>()
-    }
+    lateinit var selectedBook: MutableLiveData<ReadingBook>
 
     var selectedBookIndex: Int? = null
+        set(value) {
+            if (value != null) {
+                selectedBook = MutableLiveData<ReadingBook>(books.value?.get(value))
+            }
+            field = value
+        }
 
     init {
         val readingBookDao = AppDatabase.getDatabase(application).readingBookDao()
